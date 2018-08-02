17bf93f61106c00ea5ed799f381f2f580aa6b965
liguoqiang <331258747@qq.com>
2018-08-02 17:39:22 +0800

    versionCode versionName命名规则

--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/util/AppUtils.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/util/AppUtils.java
@@ -1,6 +1,8 @@
 package com.njz.letsgoapp.util;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 /**
  * Created by LGQ
@@ -33,5 +35,43 @@ public class AppUtils {
 		if (context != null) return context;
 		throw new NullPointerException("u should init first");
 	}
+
+
+	/**
+	 * 得到版本号code
+	 */
+	public static String getVersionCode() {
+		if(getPackageInfo() == null) return null;
+		return String.valueOf(getPackageInfo().versionCode);
+	}
+
+	/**
+	 * 得到版本name
+	 */
+	public static String getVersionName() {
+		if(getPackageInfo() == null) return null;
+		return String.valueOf(getPackageInfo().versionName);
+	}
+
+	/**
+	 * 得到包名
+	 */
+	public static String getPakgeName() {
+		if(getPackageInfo() == null) return null;
+		return getPackageInfo().packageName;
+	}
+
+	private static PackageInfo getPackageInfo() {
+		try {
+			PackageManager packageManager = getContext().getPackageManager();
+			PackageInfo packageInfo = packageManager.getPackageInfo(getContext().getPackageName(), 0);
+			return packageInfo;
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
 
 }
