dfdfad5ef572b6cd5d018ec339e5ac8cd6afbb21
liguoqiang <331258747@qq.com>
2018-08-10 18:16:01 +0800

    多图片选择

---
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/util/BuglyFileProvider.java
@@ -0,0 +1,10 @@
+package com.njz.letsgoapp.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by llt on 2017/12/13.
+ */
+
+public class BuglyFileProvider extends FileProvider {
+}
--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/mine/FeedbackActivity.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/mine/FeedbackActivity.java
@@ -1,7 +1,21 @@
 package com.njz.letsgoapp.view.mine;
 
+import android.content.Intent;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+
 import com.njz.letsgoapp.R;
 import com.njz.letsgoapp.base.BaseActivity;
+import com.njz.letsgoapp.util.accessory.PhotoAdapter;
+import com.njz.letsgoapp.util.accessory.RecyclerItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.iwf.photopicker.PhotoPicker;
+import me.iwf.photopicker.PhotoPreview;
 
 /**
  * Created by LGQ
@@ -10,6 +24,12 @@ import com.njz.letsgoapp.base.BaseActivity;
  */
 
 public class FeedbackActivity extends BaseActivity {
+
+    private RecyclerView mPhotoRecyclerView;
+    private PhotoAdapter photoAdapter;
+    private ArrayList<String> selectedPhotos = new ArrayList<>();
+
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_feedback;
@@ -19,8 +39,60 @@ public class FeedbackActivity extends BaseActivity {
     public void initView() {
         showLeftAndTitle("意见反馈");
 
+        initAddPhoto();
 
     }
+
+
+
+    //------------附件图片
+    private void initAddPhoto(){
+        //------------附件
+        mPhotoRecyclerView = $(R.id.recycler_view);
+        photoAdapter = new PhotoAdapter(context, selectedPhotos);
+        mPhotoRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(PhotoAdapter.IMAGE_LINE, OrientationHelper.VERTICAL));
+        mPhotoRecyclerView.setAdapter(photoAdapter);
+        mPhotoRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(context,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        if (photoAdapter.getItemViewType(position) == PhotoAdapter.TYPE_ADD) {
+                            PhotoPicker.builder()
+                                    .setPhotoCount(PhotoAdapter.MAX)
+                                    .setShowCamera(true)
+                                    .setPreviewEnabled(false)
+                                    .setSelected(selectedPhotos)
+                                    .start(FeedbackActivity.this);
+                        } else {
+                            PhotoPreview.builder()
+                                    .setPhotos(selectedPhotos)
+                                    .setCurrentItem(position)
+                                    .start(FeedbackActivity.this);
+                        }
+                    }
+                }));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == -1 &&
+                (requestCode == PhotoPicker.REQUEST_CODE || requestCode == PhotoPreview.REQUEST_CODE)) {
+
+            List<String> photos = null;
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+            }
+            selectedPhotos.clear();
+
+            if (photos != null) {
+
+                selectedPhotos.addAll(photos);
+            }
+            photoAdapter.notifyDataSetChanged();
+        }
+    }
+
 
     @Override
     public void initData() {
