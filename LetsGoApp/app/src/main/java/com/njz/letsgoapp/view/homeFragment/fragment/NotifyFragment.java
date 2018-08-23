253bfffa92e83835fbfe1ef39803cef0d05cd544
liguoqiang <331258747@qq.com>
2018-08-23 09:27:59 +0800

    12

--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/homeFragment/fragment/NotifyFragment.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/homeFragment/fragment/NotifyFragment.java
@@ -1,11 +1,16 @@
 package com.njz.letsgoapp.view.homeFragment.fragment;
 
+import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.njz.letsgoapp.R;
+import com.njz.letsgoapp.adapter.home.GuideListAdapter;
+import com.njz.letsgoapp.adapter.mine.FansListAdapter;
 import com.njz.letsgoapp.adapter.notify.NotifyAdapter;
 import com.njz.letsgoapp.base.BaseFragment;
+import com.njz.letsgoapp.view.home.GuideDetailActivity;
 
 /**
  * Created by LGQ
@@ -16,7 +21,7 @@ public class NotifyFragment extends BaseFragment {
 
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
-    NotifyAdapter mAdapter;
+//    NotifyAdapter mAdapter;
 
     @Override
     public int getLayoutId() {
@@ -25,8 +30,28 @@ public class NotifyFragment extends BaseFragment {
 
     @Override
     public void initView() {
+        initRecycler();
+        initSwipeLayout();
+    }
 
 
+    //初始化recyclerview
+    private void initRecycler() {
+        recyclerView = $(R.id.recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    //初始化SwipeLayout
+    private void initSwipeLayout() {
+        swipeRefreshLayout = $(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeColors(getResColor(R.color.blue));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 
     @Override
