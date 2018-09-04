11aea858c167df5cc88e2bd1f86ef59f07a02170
liguoqiang <331258747@qq.com>
2018-09-04 11:25:14 +0800

    2

--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/homeFragment/fragment/FindFragment.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/homeFragment/fragment/FindFragment.java
@@ -6,14 +6,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.njz.letsgoapp.R;
 import com.njz.letsgoapp.adapter.base.BaseFragmentAdapter;
 import com.njz.letsgoapp.base.BaseFragment;
+import com.njz.letsgoapp.util.AppUtils;
 import com.njz.letsgoapp.view.find.DynamicFragment;
 import com.njz.letsgoapp.view.find.ReleaseDynamicActivity;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +33,7 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
     private ViewPager mViewPager;
     private TextView tvCityPick, tvSearch;
     private ImageView ivRelease;
+
 
 
     private List<Fragment> mFragments;
@@ -64,6 +68,7 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
         mViewPager.setAdapter(adapter);
         mViewPager.setOffscreenPageLimit(1);
         mTabLayout.setupWithViewPager(mViewPager);
+
     }
 
     @Override
@@ -80,4 +85,54 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
                 break;
         }
     }
+    //修改tablayout  Indicator 宽度
+//    public void reflex(final TabLayout tabLayout){
+//        //了解源码得知 线的宽度是根据 tabView的宽度来设置的
+//        tabLayout.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    //拿到tabLayout的mTabStrip属性
+//                    LinearLayout mTabStrip = (LinearLayout) tabLayout.getChildAt(0);
+//
+//                    int dp10 = AppUtils.dip2px(50);
+//
+//                    for (int i = 0; i < mTabStrip.getChildCount(); i++) {
+//                        View tabView = mTabStrip.getChildAt(i);
+//
+//                        //拿到tabView的mTextView属性  tab的字数不固定一定用反射取mTextView
+//                        Field mTextViewField = tabView.getClass().getDeclaredField("mTextView");
+//                        mTextViewField.setAccessible(true);
+//
+//                        TextView mTextView = (TextView) mTextViewField.get(tabView);
+//
+//                        tabView.setPadding(0, 0, 0, 0);
+//
+//                        //因为我想要的效果是   字多宽线就多宽，所以测量mTextView的宽度
+//                        int width = 0;
+//                        width = mTextView.getWidth();
+//                        if (width == 0) {
+//                            mTextView.measure(0, 0);
+//                            width = mTextView.getMeasuredWidth();
+//                        }
+//
+//                        //设置tab左右间距为10dp  注意这里不能使用Padding 因为源码中线的宽度是根据 tabView的宽度来设置的
+//                        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) tabView.getLayoutParams();
+//                        params.width = width ;
+//                        params.leftMargin = dp10;
+//                        params.rightMargin = dp10;
+//                        tabView.setLayoutParams(params);
+//
+//                        tabView.invalidate();
+//                    }
+//
+//                } catch (NoSuchFieldException e) {
+//                    e.printStackTrace();
+//                } catch (IllegalAccessException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//    }
 }
