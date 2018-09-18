b4ad4c3c1f0542e1c7217ee02c4731a824b31be8
liguoqiang <331258747@qq.com>
2018-09-18 18:45:49 +0800

    取消订单

--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/order/OrderCancelActivity.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/view/order/OrderCancelActivity.java
@@ -1,7 +1,19 @@
 package com.njz.letsgoapp.view.order;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.njz.letsgoapp.R;
 import com.njz.letsgoapp.base.BaseActivity;
+import com.njz.letsgoapp.widget.FixedItemEditView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by LGQ
@@ -9,7 +21,15 @@ import com.njz.letsgoapp.base.BaseActivity;
  * Function: 大订单取消
  */
 
-public class OrderCancelActivity extends BaseActivity {
+public class OrderCancelActivity extends BaseActivity implements View.OnClickListener {
+
+    LinearLayout ll_reason;
+    TextView tv_reason;
+    FixedItemEditView view_name,view_phone;
+    EditText et_special;
+    List<String> reasons;
+
+
 
     @Override
     public int getLayoutId() {
@@ -19,10 +39,42 @@ public class OrderCancelActivity extends BaseActivity {
     @Override
     public void initView() {
         showLeftAndTitle("取消订单");
+
+        tv_reason = $(R.id.tv_reason);
+        ll_reason = $(R.id.ll_reason);
+        view_name = $(R.id.view_name);
+        view_phone = $(R.id.view_phone);
+        et_special = $(R.id.et_special);
+
+        ll_reason.setOnClickListener(this);
+
     }
 
     @Override
     public void initData() {
+        reasons = new ArrayList<>();
+        reasons.add("原因1");
+        reasons.add("原因2");
+        reasons.add("原因3");
+        reasons.add("原因4");
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_reason:
+                //条件选择器
+                OptionsPickerView pvOptions = new OptionsPickerBuilder(context,
+                        new OnOptionsSelectListener() {
+                            @Override
+                            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                                tv_reason.setText(reasons.get(options1));
+                            }
+                        })
+                        .build();
+                pvOptions.setPicker(reasons, null, null);
+                pvOptions.show();
+                break;
+        }
     }
 }
