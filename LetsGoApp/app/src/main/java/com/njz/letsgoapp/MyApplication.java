838b7a36fc514a03f5816c6906b3e885c60443e6
liguoqiang <331258747@qq.com>
2018-10-31 13:59:03 +0800

    bugly自定义升级框

--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/MyApplication.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/MyApplication.java
@@ -2,14 +2,17 @@ package com.njz.letsgoapp;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Environment;
 
 import com.njz.letsgoapp.constant.Constant;
-import com.njz.letsgoapp.util.log.ExceptionCrashHandler;
-import com.njz.letsgoapp.util.log.LogUtil;
-import com.njz.letsgoapp.util.SPUtils;
 import com.njz.letsgoapp.util.AppUtils;
+import com.njz.letsgoapp.util.SPUtils;
+import com.njz.letsgoapp.util.log.LogUtil;
+import com.njz.letsgoapp.view.homeFragment.HomeActivity;
+import com.njz.letsgoapp.wxapi.UpgradeDialogListener;
 import com.taobao.sophix.SophixManager;
 import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
@@ -66,9 +69,7 @@ public class MyApplication extends Application{
 
         registToWX();
 
-        //第三个参数为SDK调试模式开关 建议在测试阶段建议设置成true，发布时设置为false。
-//        CrashReport.initCrashReport(getApplicationContext(), "cd379e9015", true);
-        Bugly.init(getApplicationContext(), "a6655ed7d5", true);
+        initBugly();
 
         //sophix 热修复
         SophixManager.getInstance().queryAndLoadNewPatch();
@@ -81,5 +82,29 @@ public class MyApplication extends Application{
         // 将该app注册到微信
         mWxApi.registerApp(Constant.WEIXIN_APP_ID);
 
+    }
+
+    private void initBugly(){
+
+        /***** Beta高级设置 *****/
+        //true表示app启动自动初始化升级模块; false不会自动初始化; 开发者如果担心sdk初始化影响app启动速度，可以设置为false， 在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
+        Beta.autoInit = true;
+        //true表示初始化时自动检查升级; false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
+        Beta.autoCheckUpgrade = true;
+        //设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
+        Beta.upgradeCheckPeriod = 60 * 1000;
+        //设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
+        Beta.initDelay = 3 * 1000;
+        Beta.upgradeDialogLayoutId = R.layout.upgrade_dialog;
+        //设置sd卡的Download为更新资源保存目录;后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+        Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+       //已经确认过的弹窗在APP下次启动自动检查更新时会再次显示;
+        Beta.showInterruptedStrategy = true;
+        //只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗; 不设置会默认所有activity都可以显示弹窗;
+        Beta.canShowUpgradeActs.add(HomeActivity.class);
+        Beta.upgradeDialogLifecycleListener = new UpgradeDialogListener();
+        //第三个参数为SDK调试模式开关 建议在测试阶段建议设置成true，发布时设置为false。
+//        CrashReport.initCrashReport(getApplicationContext(), "cd379e9015", true);
+        Bugly.init(getApplicationContext(), "a6655ed7d5", true);
     }
 }
