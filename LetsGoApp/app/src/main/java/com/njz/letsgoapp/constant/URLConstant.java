9b3dfd8c8593043f2652ab42e75763b1cfffbc47
liguoqiang <331258747@qq.com>
2018-11-19 09:02:31 +0800

    删除图片选择svn

--- LetsGoApp/app/src/main/java/com/njz/letsgoapp/constant/URLConstant.java
+++ LetsGoApp/app/src/main/java/com/njz/letsgoapp/constant/URLConstant.java
@@ -8,11 +8,11 @@ package com.njz.letsgoapp.constant;
 
 public class URLConstant {
 //    public static final String BASE_URL = "http://192.168.100.156:8090/travel-framework/";//测试地址
-    public static final String BASE_URL = "http://www.njzou.net/travel-framework/";//外网地址
+//    public static final String BASE_URL = "http://www.njzou.net/travel-framework/";//外网地址
 //    public static final String BASE_URL = "http://192.168.100.122:8080/";//本地
 
     //消息本地
-//    public static final String BASE_URL = "http://192.168.100.165:8089/";//16670198324    admin
+    public static final String BASE_URL = "http://192.168.100.120:8089/";//16670198324    admin
 
     public static final String SHARE_GUIDE = BASE_URL+"/share/guideDeta.html";
     public static final String SHARE_DYNAMIC = BASE_URL+"/share/moveDeta.html";
